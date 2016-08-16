829777597766d5d4a9d4eb8497687f8785be7b83
jlperrella <jperrella@Jordans-MacBook-Pro.local>
2016-08-15 19:15:29 -0700

    First Commit
    -built basic scene elements
    -wrote ball controller script
    -wrote camera controller script

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public float launchSpeed = 400f;
+
+	private Rigidbody ball;
+	private AudioSource ballSound;
+
+	// Use this for initialization
+	void Start () {
+		ball = GetComponent<Rigidbody>();
+		ballSound = GetComponent<AudioSource> ();
+	}
+
+	void FixedUpdate () {
+		if (Input.GetKeyDown(KeyCode.Space)){
+			LaunchBall ();
+		}
+	}
+
+	public void LaunchBall () {
+		ball.velocity = new Vector3 (0, 0, launchSpeed);
+		ballSound.Play ();
+	}
+
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public Ball ball;
+
+	private Vector3 offset;
+
+
+	void Start () {
+		offset = transform.position - ball.transform.position;
+	}
+
+	void Update () {
+		transform.position = ball.transform.position + offset;
+
+		if (transform.position.z >= 1200){
+			Debug.Log ("POSITION");
+			transform.position = new Vector3(0,22,1200);
+		}
+	}
+}
