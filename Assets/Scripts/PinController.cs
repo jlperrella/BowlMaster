41e430a1607127d16981c830045a0e62683ff6f9
jlperrella <jperrella@Jordans-MacBook-Pro.local>
2016-08-22 20:07:06 -0700

    6th Commit
    -Create PinClean and PinReset animation framework
    -Wrote first draft of PinController Script

---
+++ Assets/Scripts/PinController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinController : MonoBehaviour {
+
+	public GameObject pinsPrefab;
+
+	private GameObject pins;
+	private GameObject[] individualPinArray;
+	private Rigidbody rigidBody;
+
+	public void ResetPins () {
+		Instantiate (pinsPrefab, new Vector3(0f,20f,1829f),Quaternion.identity);
+	}
+
+	public void LiftPins () {
+		individualPinArray = GameObject.FindGameObjectsWithTag("pin");
+		pins = GameObject.Find ("Pins");
+		pins.transform.Translate (0f, 25f, 0f);
+
+		foreach (GameObject pin in individualPinArray){
+			rigidBody = pin.GetComponent<Rigidbody>();
+			rigidBody.useGravity = false;
+		}
+	}
+
+	public void LowerPins () {
+		foreach (GameObject pin in individualPinArray){
+			rigidBody = pin.GetComponent<Rigidbody>();
+			rigidBody.useGravity = true;
+		}
+	}
+
+}
